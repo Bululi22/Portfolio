52a436057a8201ec2f8a6cd935ca315f0e046812
Adrian F. <adrian.ferrad@gmail.com>
2025-07-10 15:08:33 +0200

    Primer commit

---
+++ src/app/components/experiencia/experiencia.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiencia',
+  imports: [],
+  templateUrl: './experiencia.component.html',
+})
+export class ExperienciaComponent { }
