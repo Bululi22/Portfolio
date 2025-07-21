2151350f44d603f9faf16d9ce65389f3689aecdf
Adrian F. <adrian.ferrad@gmail.com>
2025-07-21 11:25:04 +0200

    Experiencia finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
-import { PruebasComponent } from './components/experiencia/pruebas/pruebas.component';
+import { PruebasComponent } from './components/pruebas/pruebas.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ExperienciaComponent, PruebasComponent],
+  imports: [
+    HeaderComponent,
+    ExperienciaComponent,
+    PruebasComponent,
+    ProyectosComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/components/proyectos/proyectos.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proyectos',
+  imports: [],
+  templateUrl: './proyectos.component.html',
+  styleUrls: ['proyectos.component.css'],
+})
+export class ProyectosComponent {}
