67d1d8a3c33c2c7b9b9a95ddcf2bcab3dd5e71f3
Adrian F. <adrian.ferrad@gmail.com>
2025-07-21 17:12:22 +0200

    Proyectos 0.1

--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-proyectos',
@@ -6,4 +6,33 @@ import { Component } from '@angular/core';
   templateUrl: './proyectos.component.html',
   styleUrls: ['proyectos.component.css'],
 })
-export class ProyectosComponent {}
+export class ProyectosComponent {
+  tarjetas = [
+    { id: 1, titulo: 'Tarjeta 1' },
+    { id: 2, titulo: 'Tarjeta 2' },
+    { id: 3, titulo: 'Tarjeta 3' },
+  ];
+
+  tarjetaSeleccionada: number | null = null;
+  animando = false;
+  animacionEntrada = false;
+
+  seleccionarTarjeta(id: number) {
+    if (this.animando) return;
+    this.animando = true;
+
+    const cambiando = this.tarjetaSeleccionada !== id;
+
+    if (this.tarjetaSeleccionada === id) {
+      this.tarjetaSeleccionada = null;
+    } else {
+      this.tarjetaSeleccionada = id;
+      this.animacionEntrada = true;
+    }
+
+    setTimeout(() => {
+      this.animando = false;
+      this.animacionEntrada = false;
+    }, 500); // coincide con la animación
+  }
+}
