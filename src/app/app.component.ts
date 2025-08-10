f4ab84bf48c4a657f4e6b926eb11c46e4ec83c05
Adrian F. <adrian.ferrad@gmail.com>
2025-08-10 17:18:44 +0200

    Portfolio v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
-import { PruebonComponent } from './components/pruebon/pruebon.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderNubesComponent } from './components/header/header-nubes/header-nubes.component';
 
--- src/app/components/pruebon/pruebon.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-pruebon',
-  imports: [],
-  styleUrls: ['./pruebon.component.css'],
-  templateUrl: './pruebon.component.html',
-})
-export class PruebonComponent {}
