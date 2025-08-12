1c2c241a08a0e239f668a31cb8a38abfe716dc9d
Adrian F. <adrian.ferrad@gmail.com>
2025-08-12 18:51:27 +0200

    Update 2

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
   copiado = false;
 
   copiarEmail() {
@@ -14,5 +14,30 @@ export class HeaderComponent {
       this.copiado = true;
       setTimeout(() => (this.copiado = false), 2000); // Oculta después de 2 seg
     });
+  }
+  ngAfterViewInit() {
+    const stars = document.querySelectorAll<SVGUseElement>('.star');
+
+    // Guardar cuándo puede volver a parpadear cada estrella
+    const cooldowns = new Map<SVGUseElement, number>();
+
+    setInterval(() => {
+      const now = Date.now();
+      const availableStars = Array.from(stars).filter(
+        (star) => !cooldowns.has(star) || cooldowns.get(star)! <= now
+      );
+
+      if (availableStars.length > 0) {
+        const star =
+          availableStars[Math.floor(Math.random() * availableStars.length)];
+
+        star.classList.add('blink');
+        cooldowns.set(star, now + 1000); // 1s de descanso antes de volver a parpadear
+
+        setTimeout(() => {
+          star.classList.remove('blink');
+        }, 500); // duración del parpadeo
+      }
+    }, 100); // cada 0.8s intentamos hacer parpadear alguna estrella
   }
 }
